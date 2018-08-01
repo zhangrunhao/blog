d18a655c0aaa7884fa4873caa30dd533f4eb2c8c
zhangrh <zhangrunhao@jingdata.com>
2018-08-01 20:55:59 +0800

    完成curry函数

---
+++ JavaScript/curry.js
@@ -0,0 +1,23 @@
+function curry(fn) {
+  let limit = fn.length
+  debugger
+  return function handleFun (...args) {
+    debugger;
+    if (args.length >= limit) {
+      return fn.apply(null, args)
+    } else {
+      return function (...arg) {
+        debugger
+        return handleFun.apply(null, args.concat(arg)) // 因为返回的这个函数, 就是handleFunction, 所以, 函数空间没有变
+      }
+    }
+  }
+}
+
+var sum = function (a, b, c) {
+  return a+b+c
+}
+
+var res = curry(sum)(1)(2)(3)
+console.log(res)
+debugger
