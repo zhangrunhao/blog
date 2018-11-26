f8e4235e95d7d7e4d363bd8d139046956676580d
zhangrh <zhangrunhao@jingdata.com>
2018-11-26 19:11:26 +0800

    dart 流程控制

--- Dart/main.dart
+++ Dart/main.dart
@@ -10,10 +10,32 @@ var image = {
   'url': '//path/to/aaa.jpg',
 };
 
+
+
 void main() {
-  print(name);
-  print(year);
-  print(antennaDiameter);
-  print(flybyObjects);
-  print(image);
+  // print(name);
+  // print(year);
+  // print(antennaDiameter);
+  // print(flybyObjects);
+  // print(image);
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (var object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while(year < 2016) {
+    print(year);
+    year += 1;
+  }
+
 }
