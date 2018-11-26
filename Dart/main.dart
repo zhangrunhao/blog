c76700a2f2f46a87f9e3ced8f5e2c96864f83988
zhangrh <zhangrunhao@jingdata.com>
2018-11-26 19:08:12 +0800

    dart 变量声明

--- Dart/main.dart
+++ Dart/main.dart
@@ -1,3 +1,19 @@
+
+
+// 变量声明
+var name = "this is name";
+var year = 1997;
+var antennaDiameter = 3.7;
+var flybyObjects = ['aaa', 'bbb', 'ccc', 'dddd'];
+var image = {
+  'tags': ['tag1', 'tag2'],
+  'url': '//path/to/aaa.jpg',
+};
+
 void main() {
-  print('Hello, World!');
+  print(name);
+  print(year);
+  print(antennaDiameter);
+  print(flybyObjects);
+  print(image);
 }
