bcb286f82f50ca255694035db55bf4cade9ae42f
zhangrh <zhangrhweb@163.com>
2019-03-08 14:00:02 +0800

    interview:加入一种新的玩法

--- Interview/17.IIFE.js
+++ Interview/17.IIFE.js
@@ -1,6 +1,6 @@
+let name = 'aa';
 (function foopp() {
-  foopp = 10
-  console.log(foopp) // function foopp() {...} // 在IIFE同名函数, JS解析器会忽略
+  name = 10
+  console.log(name) // 10
 }());
-
-console.log(foopp) // ReferenceError: foopp is not defined
+console.log(name) // 10
