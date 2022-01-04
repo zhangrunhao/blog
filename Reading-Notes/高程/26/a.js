9edcca9b6b29d8fd70d7515f817f8df7f70efb7d
zhangrh <runhaozhang@sohu-inc.com>
2022-01-04 19:27:49 +0800

    book: 高程

---
+++ Reading-Notes/高程/26/a.js
@@ -0,0 +1,12 @@
+console.log("a 文件开始")
+exports.name = "zhangrh"
+
+module.exports = {
+  name: "a 文件",
+  say: function () {
+    console.log(666)
+  }
+}
+
+
+console.log("a 文件结束")
---
+++ Reading-Notes/高程/26/b.js
@@ -0,0 +1,9 @@
+console.log('b文件 start')
+
+let a = require("./a")
+
+a = {
+  name: "bbbb"
+}
+
+console.log('b文件 end')
